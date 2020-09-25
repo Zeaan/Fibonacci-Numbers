eeeb7b30c68052a1ed8a7cc75477d3c5e7d65fdd
Zeaan Pithawala <zeaanpithawala13@gmail.com>
2020-09-25 20:54:32 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int number = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        System.out.print(a+" "+b+" ");
+
+        for(int i=2; i<number; i++){
+            int c = a+b;
+            a = b;
+            b = c;
+            System.out.print(c+" ");
+        }
+    }
+}
