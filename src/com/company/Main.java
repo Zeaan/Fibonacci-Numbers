2166526c0ec7937fda9a2fceef54cf4c67df36cc
Kerin <kerinpithawala7@gmail.com>
2020-10-02 00:06:49 +0530

    Taking care of edge case

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,20 +8,21 @@ public class Main {
 	// write your code here
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number");
-        int number = sc.nextInt();
-	if(number<0){
-        	System.out.println("Enter a positive number to compute fibonacci");
-        	number = sc.nextInt();
-	}
-        int a = 0;
-        int b = 1;
-        System.out.print(a+" "+b+" ");
+	int number = sc.nextInt();
+	if (number>0){
+        	int a = 0;
+        	int b = 1;
+        	System.out.print(a+" "+b+" ");
 
-        for(int i=2; i<number; i++){
-            int c = a+b;
-            a = b;
-            b = c;
-            System.out.print(c+" ");
-        }
+        	for(int i=2; i<number; i++){
+            		int c = a+b;
+            		a = b;
+            		b = c;
+            	System.out.print(c+" ");
+        	}
+	}
+	else{
+		System.out.println("You cannot get Finbonacci for negative integers.");
+	}
     }
 }
